48c361480741d8abd3021f02e0dd2c57f75bc3a2
W8TERM3LON <davidjiang090@gmail.com>
2019-12-19 21:31:59 -0600

    Add more email domains

--- Scammer_Spammer.py
+++ Scammer_Spammer.py
@@ -29,7 +29,7 @@ while True:
     rando3 = ''.join(random.choice(string.digits))
 
     #Chooses email format
-    emailformat = random.choice([1,2,3,4,5,6,7,8,9,10])
+    emailformat = random.choice([1,2,3,4,5,6,7,8,9,10,11])
     if emailformat == 1:        
         name = first + last + rando + rando2 + rando3
     if emailformat == 2:
@@ -50,10 +50,12 @@ while True:
         name = last + first
     if emailformat == 10:
         name = first + rando + rando2 + rando3
+    if emailformat == 11:
+        name = first + '_' + last
 
 
     #Chooses email provider
-    pickprov = random.choice([1,2,3,4,5,6])
+    pickprov = random.choice([1,1,2,3,4,5,6,7,8,9])
     if pickprov == 1:
         username = name.lower() + '@yahoo.com'
     if pickprov == 2:
@@ -63,9 +65,15 @@ while True:
     if pickprov == 4:
         username = name.lower() + '@icloud.com'
     if pickprov == 5:
-        username = name.lower() + '@gmail.com'
+        username = name.lower() + '@mail.com'
     if pickprov == 6:
         username = name.lower() + '@gmx.us'
+    if pickprov == 7:
+        username = name.lower() + '@outlook.com'
+    if pickprov == 8:
+        username = name.lower() + '@aol.com'
+    if pickprov == 9:
+        username = name.lower() + '@comcast.net'
 
     #Chooses randomly generated password between 8-16 characters long
     password = ''.join(random.choice(chars) for i in range(random.choice([8,9,10,11,12,13,14,15,16])))
